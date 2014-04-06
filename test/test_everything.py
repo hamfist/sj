f2fd1ee9cc9af4178b895a65c355fc1e272e8173
Dan Buch <dan@meatballhat.com>
2014-04-06 10:53:22 -0400

    Making tests more py.test-y, adding a smidge

--- test/test_everything.py
+++ test/test_everything.py
@@ -1,7 +1,6 @@
 # vim:fileencoding=utf-8
 import os
 import sys
-import unittest
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,20 +9,31 @@ sys.path.insert(0, os.path.dirname(HERE))
 import sj
 
 
-class Everything(unittest.TestCase):
-
-    def test_good_json_is_good(self):
-        with open(os.path.join(HERE, 'good.json')) as stream:
-            assert 0 == sj.main([], stream)
-
-    def test_also_good_json_is_also_good(self):
-        with open(os.path.join(HERE, 'also-good.json')) as stream:
-            assert 0 == sj.main([], stream)
-
-    def test_bad_json_is_bad(self):
-        with open(os.path.join(HERE, 'bad.json')) as stream:
-            assert 1 == sj.main([], stream)
+def test_good_json_is_good(capsys):
+    with open(os.path.join(HERE, 'good.json')) as stream:
+        assert 0 == sj.main(['sj'], stream)
+    out, err = capsys.readouterr()
+    assert err.strip() == ''
+    assert out.strip() == ''
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_also_good_json_is_also_good(capsys):
+    with open(os.path.join(HERE, 'also-good.json')) as stream:
+        assert 0 == sj.main(['sj'], stream)
+    out, err = capsys.readouterr()
+    assert err.strip() == ''
+    assert out.strip() == ''
+
+
+def test_bad_json_is_bad(capsys):
+    with open(os.path.join(HERE, 'bad.json')) as stream:
+        assert 1 == sj.main(['sj'], stream)
+    _, err = capsys.readouterr()
+    assert err.strip() != ''
+
+
+def test_bad_json_is_bad_but_quiet(capsys):
+    with open(os.path.join(HERE, 'bad.json')) as stream:
+        assert 1 == sj.main(['sj', '-q'], stream)
+    _, err = capsys.readouterr()
+    assert err.strip() == ''
